eae844b72c819c709c66ba460f323b27414ace88
A.J. Brown <aj@ajbrown.org>
2014-05-01 01:39:23 -0400

    Add a demo application.
    The demo application shows the the log information is sent to loggly,
    and is also still logged to the console.

---
+++ demo/app.js
@@ -0,0 +1,43 @@
+; (function(angular) {
+
+    angular.module( 'ngLoggly.demo', ['ngLoggly'] )
+
+    .constant( 
+        'logglyInputToken', 
+        '<YOUR TOKEN>' 
+    )
+
+    .config( function( LogglyLoggerProvider, logglyInputToken ) {
+        
+        LogglyLoggerProvider
+            .inputToken( logglyInputToken )
+            .useHttps( true )
+            .includeTimestamp( true )
+            .includeUrl( true )
+        ;
+
+    } )
+
+    .controller( 'MainCtrl', function( $scope, $log ) {
+        
+        $scope.inputToken = null;
+        $scope.message = '';
+
+        //We can also create named loggers, similar to log4j
+        var megaLog = $log.getLogger( 'MegaLogger' );
+
+        $scope.logIt = function() {
+            $log.info( $scope.message );
+        };
+
+        $scope.megaLogIt = function() {
+            megaLog.warn( $scope.message );
+        }
+        
+    })
+
+
+    ;
+
+
+})(window.angular);
