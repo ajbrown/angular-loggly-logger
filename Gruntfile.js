1f321de398967dd95997d024c6020d08ab56e83e
A.J. Brown <aj@ajbrown.org>
2015-02-08 20:54:47 -0500

    Use Firefox when running travis karma tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
           travis: {
             configFile: 'test/karma.conf.js',
             singleRun: true,
-            browsers: ['PhantomJS']
+            browsers: ['Firefox']
           }
       },
 
