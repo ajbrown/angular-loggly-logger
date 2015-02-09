10c0350ca1899da6abf2bdfbd3c74e55b67b4ed7
A.J. Brown <aj@ajbrown.org>
2015-02-08 21:00:15 -0500

    Fix default "test" task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-karma');
 
     grunt.registerTask('default', ['uglify'] );
-    grunt.registerTask('test', ['karma:travis:run'] );
+    grunt.registerTask('test', ['karma:travis'] );
 
 };
